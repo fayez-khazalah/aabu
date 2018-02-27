66b87447ed7581ddf9bee78673740a5f4d45cf58
Fayez <fayez.aabu@gmail.com>
2018-02-27 09:52:30 +0200

    Deleted CoursesController and Commented Related Routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@ Route::get('/pages', 'PagesController@index');
 Route::get('/services', 'PagesController@services');
 Route::get('/about', 'PagesController@about');
 
-Route::get('/courses/create', 'CoursesController@create');
-Route::post('/courses', 'CoursesController@store');
+//Route::get('/courses/create', 'CoursesController@create');
+//Route::post('/courses', 'CoursesController@store');
