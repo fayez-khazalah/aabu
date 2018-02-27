c32d90fde4ede9b58fb9b005639df48ce1222b0c
Fayez <fayez.aabu@gmail.com>
2018-02-27 22:22:34 +0200

    Updated welcome view, to make it extends the master view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-</head>
-<body>
-    {{--<h1>Welcome, {{$name}}</h1>--}}
-    {{--<h3>Your age is {{$age}}</h3>--}}
-        {{-- expr --}}
-    @foreach($data as $key => $item)
-        <h1>{{$key}}: {{$item}}</h1>
-    @endforeach
-</body>
-</html>
+@extends ('layouts.master')
+
+@section ('content')
+
+  <div class="jumbotron text-center">
+    <h1>Welcome to My Website</h1>
+  </div>
+
+@endsection
